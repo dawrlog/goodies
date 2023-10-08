dd76ccb19c0b237655fd0bd9c5baee6c83576943
D. Paes <paes@dawrlog.com>
2023-10-08 16:20:15 -0400

    Changed wkfilters.sh script to run from github.

--- wkfilters.sh
+++ wkfilters.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 pacman -S nftables sshguard --noconfirm
+curl -O https://raw.githubusercontent.com/dawrlog/goodies/main/nftables.conf
 nft -f ./nftables.conf
 sed -i 's/sshg-fw-iptables/sshg-fw-nft-sets/g' /etc/sshguard.conf
 
 systemctl enable nftables 
 systemctl enable sshguard
+
+rm -f ./nftables.conf
